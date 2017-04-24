53158fcbfae047b155cd13b2b8fe868d22e3cd6f
Avandrea <andrea.avigni1@gmail.com>
2017-04-24 12:30:34 +0200

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -1,6 +1,7 @@
 import script
+import cv2
 
-patches, _ = script.get_patches_and_labels(imagesPath="Images", PositivePointsPath="Points/PositivePoints.txt", 32, 32)
+patches, _ = script.get_patches_and_labels(50, 200)
 cv2.namedWindow("window1", 1)
-cv2.imshow(window1, patches[0])
+cv2.imshow("window1", patches[0])
 cv2.waitKey(0)
